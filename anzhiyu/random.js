ba3fc5c290ac34ab9aeff77a23f16f5eb84afddd
yangfan <fanyang@faceunity.com>
2023-12-17 16:13:37 +0800

    Site updated: 2023-12-17 16:13:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/16/hello-world/"];function toRandomPost(){
+var posts=["2023/12/17/life/about_hz_twelve people/","2023/12/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
