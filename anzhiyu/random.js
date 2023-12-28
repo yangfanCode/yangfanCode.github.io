4c3d2232f6816e763d4cfd2650ae0f149b3a0c09
yangfan <fanyang@faceunity.com>
2023-12-28 21:21:25 +0800

    Site updated: 2023-12-28 21:21:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/16/hello-world/","2023/12/20/life/about_hz_twelve_people/","2023/12/28/android/2023/curveView/","2023/12/20/moto/zhinanVillage/20221106/index/","2023/12/28/photography/basic/curve/","2023/12/20/moto/ninja400/"];function toRandomPost(){
+var posts=["2023/12/16/hello-world/","2023/12/20/life/about_hz_twelve_people/","2023/12/20/moto/ninja400/","2023/12/20/moto/zhinanVillage/20221106/index/","2023/12/28/photography/basic/curve/","2023/12/28/android/2023/curveView/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
